6c8d9d88ded579856553fdce1619001e7afabb61
Thiago Barroso da Silva <thiagoo.silva@live.com>
2021-11-25 10:13:58 -0300

    Create media.js

---
+++ media.js
@@ -0,0 +1,10 @@
+function calcularMedia (num1, num2, num3){
+ let media = ((num1 * 3) + (num2 * 3) + (num3 * 4)) / 3;
+ let resultado = ("sua nota média: " + media);
+ 
+ return resultado; 
+
+
+}
+
+calcularMedia (10,10,10)
